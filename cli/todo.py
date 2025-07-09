cca965c7f5781fcf600c859ccdf601ce6131aa57
Jinolyn <temsjinolyn@gmail.com>
2025-07-09 20:36:02 +0300

    feat(todo): add todo_list  function

--- cli/todo.py
+++ cli/todo.py
@@ -5,7 +5,7 @@ app = typer.Typer(help="Manage your to-do list")
 todo_path = "data/todos.json"
 
 @app.command()
-def add(task: str):
+def add_task(task: str):
     """Add a new task to your to-do list."""
     tasks = storage.load_data(todo_path)
     tasks.append(task)
@@ -14,4 +14,12 @@ def add(task: str):
 
 
 @app.command()
-def 
+def todo_list():
+    """List all tasks in your to-do list."""
+    tasks = storage.load_data(todo_path)
+    if not tasks:
+        typer.echo("Your to-do list is empty.")
+    else:
+        typer.echo("📝 To-Do List:")
+        for i, task in enumerate(tasks, start=1):
+            typer.echo(f"{i}. {task}")
