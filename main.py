f60a1566a07fb22fe31f457c03d59ee3d5b6b17e
Jinolyn <temsjinolyn@gmail.com>
2025-07-09 20:38:00 +0300

    chore: uncomment line to enable todo command

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = typer.Typer(help="Personal CLI Assistant ")
 
 app.add_typer(note.app, name="note", help="Manage your notes")
 app.add_typer(time_display.app, name="time", help="Display current time")
-# app.add_typer(todo.app, name="todo", help="Manage your to-do list")
+app.add_typer(todo.app, name="todo", help="Manage your to-do list")
 
 
 if __name__ == "__main__":
